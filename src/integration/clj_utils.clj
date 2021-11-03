f40c154892152923e1d5fdf550e203563a768aea
mlaine-dsg <matthew.laine@dcsg.com>
2021-11-03 16:16:50 -0400

    finish update ansible config use case

--- src/integration/clj_utils.clj
+++ src/integration/clj_utils.clj
@@ -1,6 +1,30 @@
 (ns integration.clj-utils)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(require '[cheshire.core :as json])
+
+(defn transmogrify_fields_for_ansible
+  ([param]
+   (transmogrify_fields_for_ansible param [:name]))
+  ([param fields]
+   (merge
+    (zipmap fields
+            (map (fn [field] (get param (name field))) fields))
+    {:state "present"})))
+
+(defn retrieve_key_values_from_json
+  ([json_string json_key fields]
+   (let [json (json/parse-string json_string)]
+   (->> (get json (name json_key))
+        (map #(transmogrify_fields_for_ansible % fields))))))
+
+(defn from_json_get_extra_
+  ([json json_key]
+   (from_json_get_extra_ json json_key [:name]))
+  ([json json_key fields]
+   (retrieve_key_values_from_json json json_key fields))
+  ([json json_key fields except_names]
+   (->> (from_json_get_extra_ json json_key fields)
+        (filter #(let [iname (% :name)]
+                   (every? (fn [x] (not= iname x))
+                           except_names))))))
+
--- test/integration/clj_utils_test.clj
+++ test/integration/clj_utils_test.clj
@@ -1,7 +1,42 @@
 (ns integration.clj-utils-test
   (:require [clojure.test :refer :all]
+            [clj-yaml.core :as yaml] 
             [integration.clj-utils :refer :all]))
+
+(def rabbitmq_definitions_json
+  (slurp "./resources/update_ansible_config/test/rabbitmq_definitions.json"))
+
+(def expected_rabbitmq_definitions_ansible_output
+  (slurp "./resources/update_ansible_config/test/rabbitmq_definitions_ansible_output.yaml"))
+
+(def rabbitmq_definitions_ansible_output
+  (yaml/generate-string
+   {:rabbitmq_extra_vhosts
+    (integration.clj-utils/from_json_get_extra_ rabbitmq_definitions_json
+                                                "vhosts"
+                                                [:name])
+    :rabbitmq_extra_exchanges
+    (integration.clj-utils/from_json_get_extra_ rabbitmq_definitions_json
+                                                "exchanges"
+                                                [:name
+                                                 :type
+                                                 :vhost])
+    :rabbitmq_extra_bindings
+    (integration.clj-utils/from_json_get_extra_ rabbitmq_definitions_json
+                                                "bindings"
+                                                [:destination
+                                                 :destination_type
+                                                 :routing_key
+                                                 :source
+                                                 :vhost])}
+   :dumper-options {:flow-style :block}))
+
+(deftest update_ansible_config_use_case
+  (testing "This test is for converting rabbitmq definitions json output to yaml usible by ansible to replicate necessary structures in a cluster"
+    (is (= expected_rabbitmq_definitions_ansible_output
+           rabbitmq_definitions_ansible_output))))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 0))))
+
