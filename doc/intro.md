f69955f36b925cc12f56ea5ba9c651a8f9bd1eee
mlaine-dsg <matthew.laine@dcsg.com>
2021-11-03 13:53:09 -0400

    make initial commit of clojure utils library

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj_utils
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/integration/clj_utils_test.clj
@@ -0,0 +1,7 @@
+(ns integration.clj-utils-test
+  (:require [clojure.test :refer :all]
+            [integration.clj-utils :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 0))))
